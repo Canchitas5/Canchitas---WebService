5c6776b05a59ac67c80b507f1aef971c547072dd
AlessandroChincha <89095463+AlessandroChincha@users.noreply.github.com>
2024-10-21 01:04:13 -0500

    update

--- src/main/java/com/example/Canchitas/Config/WebConfig.java
+++ src/main/java/com/example/Canchitas/Config/WebConfig.java
@@ -5,11 +5,6 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-<<<<<<< HEAD
-
-=======
->>>>>>> c0614f1 (update)
-@Configuration
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
@@ -20,8 +15,4 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addResourceHandler("/webjars/**")
                 .addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> c0614f1 (update)
